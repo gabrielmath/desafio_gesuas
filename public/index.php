55d60c5c0043c56aa4ea25bf797f499363cde60b
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2022-12-06 01:25:24 -0300

    feat(nginx): configured router

--- public/index.php
+++ public/index.php
@@ -1,24 +1,25 @@
 <?php
-
-//echo "oi";
-//exit;
 ob_start();
 
-require __DIR__ . './../src/Core/Eloquent/connect.php';
 require __DIR__ . './../vendor/autoload.php';
 
 /**
  * BOOTSTRAP
  */
 
-use CoffeeCode\Router\Router;
+use Leaf\Router;
 use Source\Core\Session;
 
 $session = new Session();
-$route = new Router(url());
 
 /**
  * WEB ROUTES
  */
-$route->namespace('\Source\App');
-$route->get("/home", "WebController:home");
+Router::setNamespace('Source\App\Controller');
+Router::get("/", "WebController@index");
+Router::post("/store", "WebController@store");
+
+/**
+ * This method executes the routes
+ */
+Router::run();
